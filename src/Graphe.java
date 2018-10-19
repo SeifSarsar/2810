85826405cee6c9d606caff7a1b8fd5a1540eef7a
Seif Sarsar <sesarb@gigl.polymtl.ca>
2018-10-19 12:49:45 -0400

    working on creerGraphe()

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -25,4 +27,44 @@ public class Graphe {
 		this.sommets = sommets;
 	}
 	
+	public void creerGraphe(BufferedReader lecteur) throws IOException {
+		
+		String ligne = lecteur.readLine();
+		if (ligne.length() > 0) {
+			
+			if (ligne.length() == 3 || ligne.length() == 4) {
+				//Creer sommet
+				creerSommet(ligne);
+			}
+			else {
+				//Relier sommet avec arc
+				
+			}
+			
+		}
+		creerGraphe(lecteur);
+	}
+	
+	public void creerSommet(String ligne) {
+		String identifiantStr = "";
+		if (ligne.length() == 4)
+			identifiantStr = ligne.substring(0,2);
+		else
+			identifiantStr = ligne.substring(0,1);
+		
+		int identifiant = Integer.parseInt(identifiantStr);
+		boolean estRechargeable = (ligne.charAt(2) == '0') ? false : true;
+		Sommet nouveauSommet = new Sommet(identifiant,estRechargeable);
+		sommets.put(identifiant, nouveauSommet);
+	}
+	
+	public void relierSommet(String ligne) {
+		
+		String sommetStr = ligne.substring(0, 2);
+		//Si sommetStr contient une virgule
+		if (sommetStr.charAt(sommetStr.length()-1) == ',')
+			//decouper
+			sommetStr = sommetStr.substring(0, 1);
+	
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Graphe graphe = new Graphe();
+		
+		File fichier = new File("centresLocaux.txt");
+		
+		BufferedReader lecteur = new BufferedReader(new FileReader(fichier));
+		
+		graphe.creerGraphe(lecteur); //NOM FICHIER PAS EN PARAMETRE
+		lecteur.close();
 	}
 
 }
